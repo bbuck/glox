290906a238792036b9328b16b34895c9b40d8fd7
Brandon Buck <lordizuriel@gmail.com>
2018-12-02 12:34:46 -0600

    complete a challenge from chapter 5 (i think)

--- tree/printer/printer_test.go
+++ tree/printer/printer_test.go
@@ -8,18 +8,29 @@ import (
 	"github.com/bbuck/glox/tree/printer"
 )
 
+var ex = expr.NewBinary(
+	expr.NewUnary(
+		token.New(token.Minus, "-", nil, 1),
+		expr.NewLiteral(expr.NumberLiteral, float64(123)),
+	),
+	token.New(token.Star, "*", nil, 1),
+	expr.NewGrouping(
+		expr.NewLiteral(expr.NumberLiteral, float64(45.67)),
+	),
+)
+
 func Test_Print(t *testing.T) {
-	ex := expr.NewBinary(
-		expr.NewUnary(
-			token.New(token.Minus, "-", nil, 1),
-			expr.NewLiteral(expr.NumberLiteral, float64(123)),
-		),
-		token.New(token.Star, "*", nil, 1),
-		expr.NewGrouping(
-			expr.NewLiteral(expr.NumberLiteral, float64(45.67)),
-		),
-	)
-	if printer.Print(ex) != "(* (- 123) (group 45.67))" {
-		t.Fail()
+	result := printer.Print(ex)
+	expected := "(* (- 123) (group 45.67))"
+	if result != expected {
+		t.Errorf("expected %q but got %q", expected, result)
+	}
+}
+
+func Test_PrintRPN(t *testing.T) {
+	expected := "123 - 45.67 *"
+	result := printer.PrintRPN(ex)
+	if result != expected {
+		t.Errorf("expected %q but got %q", expected, result)
 	}
 }
---
+++ tree/printer/rpn.go
@@ -0,0 +1,54 @@
+package printer
+
+// reverse polish notation printer
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/bbuck/glox/tree/expr"
+)
+
+type rpnPrinter struct {
+	buf *bytes.Buffer
+}
+
+// PrintRPN will walk the expression tree and print the operations in
+// reverse polish notation (with the operation after the numbers)
+func PrintRPN(e expr.Expr) string {
+	printer := &rpnPrinter{
+		buf: new(bytes.Buffer),
+	}
+	e.Accept(printer)
+
+	return printer.buf.String()
+}
+
+func (p *rpnPrinter) VisitBinary(b *expr.Binary) {
+	p.parenthesize(b.Operator.Lexeme, b.Left, b.Right)
+}
+
+func (p *rpnPrinter) VisitUnary(u *expr.Unary) {
+	p.parenthesize(u.Operator.Lexeme, u.Right)
+}
+
+func (p *rpnPrinter) VisitLiteral(l *expr.Literal) {
+	if l.Value == nil {
+		p.buf.WriteString("nil")
+		return
+	}
+
+	p.buf.WriteString(fmt.Sprintf("%v", l.Value))
+}
+
+func (p *rpnPrinter) VisitGrouping(g *expr.Grouping) {
+	g.Expression.Accept(p)
+}
+
+func (p *rpnPrinter) parenthesize(name string, es ...expr.Expr) {
+	for _, e := range es {
+		e.Accept(p)
+		p.buf.WriteRune(' ')
+	}
+	p.buf.WriteString(name)
+}
